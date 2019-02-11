d0d01d6dcc40875c739252d8b4d60b103a2ce614
tangyue <tangyue@gosotech.com>
2019-02-11 15:41:27 +0800

    新增实体

---
+++ src/main/java/service/sys/DeptService.java
@@ -0,0 +1,18 @@
+/**
+ * chenxitech.cn Inc. Copyright (c) 2017-2019 All Rights Reserved.
+ */
+package service.sys;
+
+import db.JdbcRepositoryWrapper;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ *
+ * @author tangyue
+ * @version $Id: DeptService.java, v 0.1 2019-02-10 15:24 tangyue Exp $$
+ */
+@Slf4j
+public class DeptService extends JdbcRepositoryWrapper {
+
+    private static final String BASE = "";
+}
---
+++ src/main/java/service/sys/OrgService.java
@@ -0,0 +1,16 @@
+/**
+ * chenxitech.cn Inc. Copyright (c) 2017-2019 All Rights Reserved.
+ */
+package service.sys;
+
+import db.JdbcRepositoryWrapper;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ *
+ * @author tangyue
+ * @version $Id: OrgService.java, v 0.1 2019-02-10 15:26 tangyue Exp $$
+ */
+@Slf4j
+public class OrgService extends JdbcRepositoryWrapper {
+}
